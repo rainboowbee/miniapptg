d8d5329c871bf5b33640236f7bf7227274f6277b
rainboowbee <rainboowbee@gmail.com>
2025-08-23 00:03:05 +0300

    fix ton connect

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import { useState } from 'react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -14,8 +15,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }))
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </TonConnectUIProvider>
   )
 }
